e709e73d11e506e67a96de20b36f4dc764b83829
David Davies <David_Davies@mail.com>
2022-02-13 21:11:24 +0000

    typical file sturcture added with common or typical code for the server and config.  also added blog model and schema, seeds

---
+++ server.js
@@ -0,0 +1,43 @@
+// Dependencies
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+
+// Sets up the Express App
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set up sessions with cookies and keep it for a day.
+const sess = {
+  secret: "User session",
+  cookie: {
+    maxAge: 86400,
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+// Set Handlebars as the default template engine.
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(routes);
+
+// Starts the server to begin listening
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`\nServer running on port ${PORT}. `));
+});
