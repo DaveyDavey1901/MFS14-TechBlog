6234b798a990e4ec5eb1902408a7f066306d1816
David Davies <David_Davies@mail.com>
2022-02-17 00:10:07 +0000

    added the index routes and andded some comments to models

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 
 const blogRoutes = require("./blogRoutes");
+const homeRoutes = require("./home-routes")
 
 router.use("/blogs", blogRoutes);
+router.use("/", homeRoutes);
 
 module.exports = router;
-
 
 
--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@ const User = require("./User");
 const Blog = require("./Blog");
 const Comment = require("./Comment");
 
+//user relationship user can have many blogs and can have many comments
 User.hasMany(Blog, {
   foreignKey: "userId",
   onDelete: "CASCADE",
@@ -11,7 +12,7 @@ User.hasMany(Comment, {
   foreignKey: "userId",
   onDelete: "CASCADE",
 });
-
+//blog relationship blogs are written by one user. and a blog can have many comments.
 Blog.belongsTo(User, {
   foreignKey: "userId",
   onDelete: "CASCADE",
@@ -21,7 +22,7 @@ Blog.hasMany(Comment, {
   foreignKey: "blogId",
   onDelete: "CASCADE",
 });
-
+//comments relationship comments are written by one user and they belong to one blog.
 Comment.belongsTo(User, {
   foreignKey: "userId",
 });
