2f6b1d1e4fcd107034cb36f1757124d2cc57ef1d
David Davies <David_Davies@mail.com>
2022-02-17 21:52:27 +0000

    added login

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 
 const blogRoutes = require("./addUpdateBlog.js");
-
+const userRoutes = require("./userRoutes");
 
 router.use("/blogs", blogRoutes);
-
+router.use("/users", userRoutes);
 
 module.exports = router;
