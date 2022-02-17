1b9f7553cbeff2fcf6c83d2250c62089bf17921c
David Davies <David_Davies@mail.com>
2022-02-17 17:24:59 +0000

    added a new blog hbs and a new.js to allow you to create new posts

---
+++ public/js/new.js
@@ -0,0 +1,27 @@
+async function newFormHandler(event) {
+  event.preventDefault();
+  const title = document.querySelector("#blog_title").value;
+  const contents = document.querySelector("#blog_contents").value;
+  
+  const response = await fetch(`/blogs`, {
+    method: "POST",
+    body: JSON.stringify({
+      title,
+      contents,
+      
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  
+  if (response.ok) {
+    document.location.replace("/blogs");
+  } else {
+    alert("Failed to add new tech blog");
+  }
+}
+
+document
+  .querySelector(".new-blog-form")
+  .addEventListener("submit", newFormHandler);
