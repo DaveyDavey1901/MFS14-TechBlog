f411f951e6e2600f414d1560831cb847d8370478
David Davies <David_Davies@mail.com>
2022-02-18 17:28:01 +0000

    changed route so it only showed all the current user logs

--- controllers/dashRoutes.js
+++ controllers/dashRoutes.js
@@ -2,8 +2,9 @@ const router = require("express").Router();
 const { Blog, User } = require("../models/");
 const withAuth = require("../utils/auth");
 
+
 router.get("/", withAuth, (req, res) => {
-  res.render("homeloggedIn", {
+    res.render("homeloggedIn", {
     layout: "dashboard",
   });
 });
@@ -15,10 +16,13 @@ router.get("/new", withAuth, (req, res) => {
 });
 
 
-//get all blogs
+//get all blogs that the user has created so far.
 router.get("/blogs",withAuth, async (req, res) => {
   try {
-    const blogs = (await Blog.findAll({ include: [User] })).map((blog) =>
+    const blogs = (await Blog.findAll({
+      where:{"userId": req.session.userId},
+      include: [User]
+    })).map((blog) =>
       blog.get({ plain: true })
     );
     res.render("blogsLoggedIn", { layout: "dashboard", blogs });
